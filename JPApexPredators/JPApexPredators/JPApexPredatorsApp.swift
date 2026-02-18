b2df236c8568340d645055e51367d4c3bdf9f464
Liesel Koeb <lieselmatos@gmail.com>
2026-02-18 11:55:16 -0300

    Lesson 1 - Create JP Apex Predators Project - Project setup - App resources - JSON data

---
+++ JPApexPredators/JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Liesel Matos Koeb on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
