3c84a1a2946419c5cff2396d3232d364e91b6fb7
Liesel Koeb <lieselmatos@gmail.com>
2026-02-22 18:18:51 -0300

    Lesson 9 - Predator Detail - Preview modifiers - ScrollView - GeometryReader - ScaleEffect - Offset

---
+++ JPApexPredators/JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,48 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Liesel Matos Koeb on 22/02/26.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                ZStack (alignment: .bottomTrailing) {
+                    // Background image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    // Dino image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
+                
+                // Dino name
+                
+                // Current location
+                
+                // Appears in
+                
+                // Movie moment
+                
+                // Link to webpage
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    PredatorDetail(predator: Predators().apexPredators[2])
+}
